542a11d380be8dbe07bfc5a657e1ccdc6f3e0671
SEBS_MGZZ <sebastian.mgzz@outlook.com>
2020-03-23 14:39:17 -0600

    timeline images

--- dynamic/index.js
+++ dynamic/index.js
@@ -6,11 +6,7 @@ const scrollerMonth = document.querySelector("#scroller h3");
 const rows = document.querySelectorAll("#main > div:not(:last-child)")
 
 window.onscroll = function (e) {  
-    let thresholdPosition = window.scrollY + window.innerHeight - 300;
-
-    scrollerYear.innerText = window.scrollY;
-    scrollerMonth.innerText = window.scrollY + window.innerHeight;
-
+    let thresholdPosition = window.scrollY + window.innerHeight - 200;
     for (let i = 1; i < rows.length; i++ ) {
         let rowTopPosition = rows[i].offsetTop;
         if (thresholdPosition > rowTopPosition) {
