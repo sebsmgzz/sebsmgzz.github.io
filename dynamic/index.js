7c75c07ce57c5a7abe2d90ffb38b2b11e1e3aa4f
SEBS_MGZZ <sebastian.mgzz@outlook.com>
2020-03-25 09:55:50 -0600

    fixed tree trunk

--- dynamic/index.js
+++ dynamic/index.js
@@ -1,23 +1,43 @@
 
+// DOM elements
 const scrollerUp = document.querySelector("#scroller button:first-child");
 const scrollerDown = document.querySelector("#scroller button:last-child");
 const scrollerYear = document.querySelector("#scroller h1");
 const scrollerMonth = document.querySelector("#scroller h3");
 const rows = document.querySelectorAll("#main > div:not(:last-child)")
 
+// Local variables
+let scrollerRow = 1;
+
+// Events
 window.onscroll = function (e) {  
     let thresholdBottom = window.scrollY + window.innerHeight;
     let thresholdTop = window.scrollY;
     for (let i = 1; i < rows.length; i++ ) {
         let rowTopPosition = rows[i].offsetTop;
         let rowBottomPosition = rows[i].offsetTop + rows[i].offsetHeight;
+        let leftContent = rows[i].children[0];
+        let rightContent = rows[i].children[2];
+        let image = rows[i].children[1].children[0];
+        let year = rows[i].querySelector("h2");
+        let month = rows[i].querySelector("h4");
         if (thresholdBottom > rowTopPosition) {
-            rows[i].classList.remove("hiddenRow");
-            rows[i].classList.add("visibleRow");
+            leftContent.classList.remove("hidden");
+            rightContent.classList.remove("hidden");
+            image.classList.remove("hidden");
+            leftContent.classList.add("visible");
+            rightContent.classList.add("visible");
+            image.classList.add("visible");
+            scrollerYear.innerText = year.innerText;
+            scrollerMonth.innerText = month.innerText;
         }
         if (rowTopPosition > thresholdBottom || rowBottomPosition < thresholdTop ) {
-            rows[i].classList.remove("visibleRow");
-            rows[i].classList.add("hiddenRow");
+            leftContent.classList.remove("visible");
+            rightContent.classList.remove("visible");
+            image.classList.remove("visible");
+            leftContent.classList.add("hidden");
+            rightContent.classList.add("hidden");
+            image.classList.add("hidden");
         }
     }
 }
@@ -45,3 +65,5 @@ scrollerDown.addEventListener( "click", (e) => {
         }
     }
 });
+
+// Functions
