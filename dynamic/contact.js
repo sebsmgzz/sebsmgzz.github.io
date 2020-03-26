fdd23d61e69da7ebe791ab2a8ddb11064e1fb902
SEBS_MGZZ <sebastian.mgzz@outlook.com>
2020-03-26 13:43:31 -0600

    functioning carousel

--- dynamic/contact.js
+++ dynamic/contact.js
@@ -1,6 +1,10 @@
 
 // DOM elements
+const main = document.querySelector("#main");
 const bullets = document.querySelector(".bullets");
+
+// Locals
+let carouselIndex = 0;
 
 // Event listeners
 for (let i = 0; i < bullets.children.length; i++) {
@@ -10,14 +14,44 @@ for (let i = 0; i < bullets.children.length; i++) {
 // Functions
 function bulletClicked() {
 
-    // Inactivate all bullets
+    // Find my index
+    for (let i = 0; i < bullets.children.length; i++) {
+        if ( bullets.children[i] == this) {
+            toggleBullet(i);
+        }
+    }
+
+}
+function toggleBullet(bulletIndex) {
+
+    // Inactivate everything
     for (let i = 0; i < bullets.children.length; i++) {
         bullets.children[i].classList.remove("active");
         bullets.children[i].classList.add("inactive");
     }
 
-    // Activate clicked
-    this.classList.remove("inactive");
-    this.classList.add("active");
+    // Activate index
+    bullets.children[bulletIndex].classList.remove("inactive");
+    bullets.children[bulletIndex].classList.add("active");
+
+    // Updated background
+    let covertIndex = bulletIndex+1;
+    main.className = "cover" + covertIndex;
 
 }
+function carousel()
+{
+
+    // Update index for carousel
+    carouselIndex++;
+    if (carouselIndex >=  bullets.children.length) {
+        carouselIndex = 0;
+    }
+
+    // Apply update
+    toggleBullet(carouselIndex);
+
+}
+
+// Intervals
+setInterval(carousel, 2500);
