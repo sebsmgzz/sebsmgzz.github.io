98d7ffaa8830a75edb6ea6ee60a0e84eed71770a
SEBS_MGZZ <sebastian.mgzz@outlook.com>
2020-03-26 18:48:41 -0600

    full screen carousel

--- dynamic/contact.js
+++ dynamic/contact.js
@@ -1,56 +1,107 @@
 
 // DOM elements
 const main = document.querySelector("#main");
-const bullets = document.querySelector(".bullets");
+const titles = document.querySelectorAll(".title");
+const contents = document.querySelectorAll(".content");
+const backgrounds = document.querySelectorAll(".background");
+const bullets = document.querySelectorAll(".bullet");
 
 // Locals
-let carouselIndex = 0;
+let globalIndex = 0;
 
 // Event listeners
-for (let i = 0; i < bullets.children.length; i++) {
-    bullets.children[i].addEventListener( "click", bulletClicked);
-}
+bullets.forEach( (bullet) => bullet.addEventListener("click",bulletClicked));
 
-// Functions
+// Event functions
 function bulletClicked() {
 
     // Find my index
-    for (let i = 0; i < bullets.children.length; i++) {
-        if ( bullets.children[i] == this) {
+    for (let i = 0; i < bullets.length; i++) {
+
+        // Toggle with my index
+        if ( bullets[i] == this) {
             toggleBullet(i);
+            toggleCarousel(i);
+            globalIndex++;
         }
+
     }
 
 }
+
+// Inner functions
 function toggleBullet(bulletIndex) {
 
     // Inactivate everything
-    for (let i = 0; i < bullets.children.length; i++) {
-        bullets.children[i].classList.remove("active");
-        bullets.children[i].classList.add("inactive");
-    }
-
-    // Verify index for carousel
-    carouselIndex = bulletIndex;
-    if (carouselIndex >=  bullets.children.length) {
-        carouselIndex = 0;
+    for (let i = 0; i < bullets.length; i++) {
+        bullets[i].classList.remove("active");
+        bullets[i].classList.add("inactive");
     }
 
     // Activate index
-    bullets.children[carouselIndex].classList.remove("inactive");
-    bullets.children[carouselIndex].classList.add("active");
-
-    // Updated carousel index
-    carouselIndex++;
-
-    // Updated background
-    main.className = "cover" + carouselIndex;
+    bullets[bulletIndex].classList.remove("inactive");
+    bullets[bulletIndex].classList.add("active");
 
 }
-function toggleCarousel()
-{
-    toggleBullet(carouselIndex);
+function toggleCarousel(carouselIndex) {
+
+    // Iterate per elements
+    for (let i = 0; i < titles.length; i++) {
+
+        // Declare variables
+        let classToRemoveA;
+        let classToRemoveB;
+        let classToAdd;
+
+        // Move to left
+        if (i < carouselIndex) {
+            classToRemoveA = "center";
+            classToRemoveB = "right";
+            classToAdd = "left";
+        }
+
+        // Move to center
+        if ( i == carouselIndex) {
+            classToRemoveA = "left";
+            classToRemoveB = "right";
+            classToAdd = "center";
+        }
+
+        // Move to right
+        if( i > carouselIndex) {
+            classToRemoveA = "left";
+            classToRemoveB = "center";
+            classToAdd = "right";
+        }
+
+        // Update
+        titles[i].classList.remove(classToRemoveA,classToRemoveB)
+        contents[i].classList.remove(classToRemoveA,classToRemoveB)
+        backgrounds[i].classList.remove(classToRemoveA,classToRemoveB)
+        titles[i].classList.add(classToAdd)
+        contents[i].classList.add(classToAdd)
+        backgrounds[i].classList.add(classToAdd)
+
+    }
+
 }
 
 // Intervals
-setInterval(toggleCarousel, 2500);
+function nextCarouselBullet()
+{
+
+    // Get next carousel index
+    ++globalIndex;
+
+    // Verify index for carousel
+    if (globalIndex >=  bullets.length) {
+        carouselIndex = 0;
+    }
+
+    // Update
+    toggleBullet(globalIndex);
+    toggleCarousel(globalIndex);
+
+}
+
+//setInterval(nextCarouselBullet, 2500);
