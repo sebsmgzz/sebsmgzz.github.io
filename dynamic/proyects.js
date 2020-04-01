46c66a4c1ff1273204b5c125610e5bed474b2a51
SEBS_MGZZ <sebastian.mgzz@outlook.com>
2020-04-01 16:34:36 -0600

    Transitions

---
+++ dynamic/proyects.js
@@ -0,0 +1,20 @@
+
+// DOM elements
+const columns = document.querySelectorAll(".column");
+const contents = document.querySelectorAll(".content");
+
+// Event listeners
+columns.forEach( (column) => column.addEventListener("click",columnClicked));
+
+// Event functions
+function columnClicked() {
+    this.classList.toggle("fullsize")
+    for(let i = 0; i < columns.length; i++) {
+        if (columns[i] != this) {
+            columns[i].classList.toggle("hidden");
+        } else {
+            contents[i].classList.toggle("show")
+        }
+    }
+}
+
