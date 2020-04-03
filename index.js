f77c649a551dd3169ba4c20afe3e4ecd37310bcd
SEBS_MGZZ <sebastian.mgzz@outlook.com>
2020-04-03 15:06:02 -0600

    landing

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// DOM Elements
+const anchores = document.querySelectorAll("a");
+const icons = document.querySelectorAll(".icon");
+
+// Event listeners
+anchores.forEach( (anchore) => anchore.addEventListener("mouseenter",anchoreHovered));
+
+// Event functions
+function anchoreHovered() {
+    const icon = this.querySelector(".icon");
+    console.log(icon);
+    icon.classList.toggle("hovered");
+}
