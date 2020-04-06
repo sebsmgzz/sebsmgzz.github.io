0707ef10592f7bdc0d959bd53b5b00e3d9acb09c
SEBS_MGZZ <sebastian.mgzz@outlook.com>
2020-04-06 17:44:10 -0500

    global links updated

--- career/career.js
+++ career/career.js
@@ -1,43 +1,20 @@
 
 // DOM elements
-const dropdowns = document.querySelectorAll(".dropdown");
-const sections = document.querySelectorAll(".section");
-const plusIcons = document.querySelectorAll(".icons > i.fa-plus-square");
-const minusIcons = document.querySelectorAll(".icons > i.fa-minus-square");
+const containers = document.querySelectorAll(".container");
+const contents = document.querySelectorAll(".contents");
 
 // Event listeners
-dropdowns.forEach( (dropdown) => dropdown.addEventListener("click", dropdownClicked));
+containers.forEach( (container) => container.addEventListener("click",containerClicked));
 
 // Event functions
-function dropdownClicked() {
-    
-    // Find section index
-    for (let i = 0; i < dropdowns.length; i++) {
-
-        // Toggle
-        if ( dropdowns[i] == this) {
-            toggleSection(i);
-        }
-
-    }
-
-}
-
-// Inner functions
-function toggleSection(sectionIndex) {
-
-    // Deactivate everything but indexed
-    for (let i = 0; i < sections.length; i++) {
-        if (i != sectionIndex) {
-            sections[i].classList.remove("active");
-            plusIcons[i].classList.add("on");
-            minusIcons[i].classList.remove("on");
+function containerClicked() {
+    this.classList.toggle("fullsize");
+    for(let i = 0; i < containers.length; i++) {
+        if (containers[i] != this) {
+            containers[i].classList.toggle("hidden");
+        } else {
+            contents[i].classList.toggle("show");
         }
     }
-
-    // Toggle indexed
-    sections[sectionIndex].classList.toggle("active");
-    plusIcons[sectionIndex].classList.toggle("on");
-    minusIcons[sectionIndex].classList.toggle("on");
-
 }
+
