ab17342975db17417a2ae96ba98c9fe075df5a86
SEBS_MGZZ <sebastian.mgzz@outlook.com>
2020-04-06 19:30:46 -0500

    dropdown

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,7 @@
 
 // DOM elements
 const dropdowns = document.querySelectorAll(".dropdown");
-const sections = document.querySelectorAll(".section");
+const contents = document.querySelectorAll(".contents");
 const plusIcons = document.querySelectorAll(".icons > i.fa-plus-square");
 const minusIcons = document.querySelectorAll(".icons > i.fa-minus-square");
 
@@ -11,33 +11,6 @@ dropdowns.forEach( (dropdown) => dropdown.addEventListener("click", dropdownClic
 // Event functions
 function dropdownClicked() {
     
-    // Find section index
-    for (let i = 0; i < dropdowns.length; i++) {
-
-        // Toggle
-        if ( dropdowns[i] == this) {
-            toggleSection(i);
-        }
-
-    }
-
-}
-
-// Inner functions
-function toggleSection(sectionIndex) {
-
-    // Deactivate everything but indexed
-    for (let i = 0; i < sections.length; i++) {
-        if (i != sectionIndex) {
-            sections[i].classList.remove("active");
-            plusIcons[i].classList.add("on");
-            minusIcons[i].classList.remove("on");
-        }
-    }
-
-    // Toggle indexed
-    sections[sectionIndex].classList.toggle("active");
-    plusIcons[sectionIndex].classList.toggle("on");
-    minusIcons[sectionIndex].classList.toggle("on");
+    this.classList.toggle("open");
 
 }
