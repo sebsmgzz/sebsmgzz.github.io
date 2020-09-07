7fc1ed11b76dde447173319fa901fb43a70be251
SEBS_MGZZ <sebastian.mgzz@outlook.com>
2020-09-07 18:03:38 -0500

    chore: Added more education inputs

--- pages/career/data.js
+++ pages/career/data.js
@@ -4,24 +4,72 @@ var careerData = [
         position: "Electric Harness Designer",
         company: "John Deere",
         dates: "September 2019 - June 2020",
-        description: "Development of technical drawings and 3D CAD models as well as their integration into the vehicle. Development of digital tools for processes automation."
+        description: "As an electric harness designer, I was assigned to several teams to provide support in technical drawings and 3D CAD models development. Also, I provided a digital solution to aid in the design of these drawings to avoid recurrent steps of the process and help reach compliance of standards. The solution included a C# implementation through which the engineers could load a data report from their CAD design software to be analysed and then get feedback on their design in realtion to the compliance policies. Also, when determining the cost of components from different providers, another tool (a web scrapper) was developed to help gather the correct information throughout the web and reduce time in proyect cost estimations."
     },
     {
         position: "VB developer",
         company: "Bioana",
         dates: "August 2019 - March 2020",
-        description: "Design and development of desktop applications for optimizing methodologies in plastic injection. General electrician. "
+        description: "As a visual basic developer, I started in the company to develop a digital solution to automate the manufacture design of plastic inyection molds. The solution was a desktop application which automated the input informat from the current design, and after selecting the desired platic inyection machine, the parameters for the calculations were loaded from a local database and performed the corresponding markups on the inputs to signal the faults or possible improvements for the process. Allowing the user (design engineer) to have instant feedback on their design, select the manufacture machine more properly, and increase the quality on the resulting products."
+    },
+    {
+        position: "BMES volunteer and main collaborator",
+        company: "BMES - BioMedial Engineer Society",
+        dates: "January 2019 - June 2019",
+        description: "During the volunteer, I was the main collaborator and my role included networking and providing human resources for the society in order to develop stand up events. These was achieved through my contacts network and by investigating and getting in touch with different volunteers from other societies."
+    },
+    {
+        position: "Process engineer intern",
+        company: "EMIS - Medical and Wellness Sciences Shcool",
+        dates: "January 2019 - June 2019",
+        description: "As a process engineer intern, I was assigned to a team to help determine and optimize the working parameters of a 3D printer. The printer was working with a new type of polymer previously designed by another team for innovative solution in the protesis manufacture area. The printing optimization of the polymer was achieved through extensive testing for data gathering to develop a mathematic model for each parameter. After the models were obtained, the point of optimization was easily obtained."
+    },
+    {
+        position: "Dron design proyect",
+        company: "Foro IDEEA - Internal competition",
+        dates: "January 2019 - June 2019",
+        description: "The competition consisted on a conceptual design of a drone to solve a given problematic (Search & Rescue). The design was done through the 'Design Thinking' process, and the separation of concers was achieved through separation of tasks among members of the team and mocking up the best ideas each teammate had. These decisions were made through argumentation and during online meetings of the team."
+    },
+    {
+        position: "PrepaNet Math teacher volunteer",
+        company: "PrepaNet @NET",
+        dates: "July 2018 - December 2018",
+        description: "As a match teacher, I was providing on math classes to low-resources people. These was people from all ages, from teenagers to adults and the class was online thorugh resources provided by the organization PrepaNet. I had a group of around 10 persons who desired to learn mostly about trigonometry and equation solving topics."
     },
     {
         position: "Data Analysis Developer",
         company: "Negocio 360",
         dates: "May 2018 - August 2018",
-        description: "Design and implementation of methodologies for optimizing the analysis of data. Development of UI and integration with backend. "
+        description: "As a data analysis developer, I was assigned the task of reestructuring a current data on a model the company already had for estimating the amount of sales to be done the next month. This was based on a set of data of previous records from the company. The implementation of the solution included the design of an interface to input the data and choose between different types of estimations (Linear, polynomial, exponential, etc), to determine a more accurate approach. This was done through VB6."
+    },
+    {
+        position: "Community manager volunteer",
+        company: "Clinicas del azucar (Sugar clinics)",
+        dates: "February 2018 - August 2018",
+        description: "As a volunteer I was in charge of managing the social networks of the organization, as well as uploading content related to wellness and good eating habits. All of the content uploaded had to be uploaded with their corresponding information sources for veracity of the organization and to avoid plagiarism."
+    },
+    {
+        position: "IKNEXTIA volunteer and main collaborator",
+        company: "IKNEXTIA",
+        dates: "January 2018 - June 2018",
+        description: "During the volunteer, I was the main collaborator reporting to the directives of the society of the different events and tasks done in name of the society. These included managing the personel and other volunteers as well as assigning them tasks and follow ups on their work and schedule. I also had to make sure proyects where achieved on schedule and that the quality of them is of what it was expected."
+    },
+    {
+        position: "Electric motor coiling proyect",
+        company: "ITESM",
+        dates: "January 2018 - May 2018",
+        description: "During the proyect, I was given the hands-on tasks of coiling and repairing electric motors. These was usually done thorugh a special machinery for coiling, although manually coiling was often required."
     },
     {
         position: "General mechanic",
         company: "DIMEX",
         dates: "May 2017 - July 2017",
-        description: "General maintenance to combustion engines and repair of electromechanical components."
-    }
+        description: "As a general mechanic, I had hands-on tasks such as air filter and oil replacement in several machinery such as: Air compressors, generators, tractors, etc. I also had to deal with problem solving without knowing the cause of the problem, which included a lot of critical thinking."
+    },
+    {
+        position: "CVA Computer teacher volunteer",
+        company: "CVA - Virtual Learning Center",
+        dates: "July 2016 - December 2016",
+        description: "As a computer teacher volunteer, I provided support in teaching about computer and technology usage to adults of age above 40s that struggled to learn about these resources. The knowledge aquired help them organize their virtual documents, understand the technological terms as well as learning how to use these technologies. Mostly to get in touch with their family and friends."
+    },
 ];
