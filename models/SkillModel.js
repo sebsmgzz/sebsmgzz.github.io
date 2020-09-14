b3a9dc8d3ddfc18809adb1b8de2e0d008ffffea4
SEBS_MGZZ <sebastian.mgzz@outlook.com>
2020-09-14 12:42:32 -0500

    chore: JSDoc comment

--- models/SkillModel.js
+++ models/SkillModel.js
@@ -1,5 +1,5 @@
 /**
-*    Represents a skill
+*    @class Represents a skill
 *    @param name {String} The name representing the skill
 *    @param imagePath {String} The path of the image representing the skill
 *    @param imagePath {String} The alternative text of the image
