7ac1c8c73ad49a3f57dc4c2504b7c8f5603f3b41
sebsmgzz <sebastian.mgzz@outlook.com>
2023-05-11 22:35:25 -0600

    feat: Add home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,4 @@
+import myself from "resources/images/myself.png";
 import "./HomePage.scss";
 
 export type HomePageProps = {
@@ -6,8 +7,21 @@ export type HomePageProps = {
 
 export const HomePage = function(props: HomePageProps) {
     return (
-        <div>
-            HomePage
+        <div className="container col-xxl-8 px-4 py-5">
+            <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                <div className="col-10 col-sm-8 col-lg-6 m-auto">
+                    <img src={myself} 
+                        className="d-block mx-lg-auto img-fluid m-auto" 
+                        alt="Bootstrap Themes" 
+                        loading="lazy" />
+                </div>
+                <div className="col-lg-6">
+                    <h1 className="display-5 fw-bold lh-1 mb-3 text-center">Sebastián M. Gzz.</h1>
+                    <p className="lead text-center">Cloud architect</p>
+                    <p className="lead text-center">Full stack developer</p>
+                    <p className="lead text-center">Electric engineer</p>
+                </div>
+            </div>
         </div>
     );
 }
