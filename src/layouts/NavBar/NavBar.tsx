e34f3f5ee3cacde73be7a5acf2cc5f946a3c6926
sebsmgzz <sebastian.mgzz@outlook.com>
2023-05-16 16:00:32 -0600

    fix: Keep id in state

--- src/layouts/NavBar/NavBar.tsx
+++ src/layouts/NavBar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { endpoints } from "global/constants";
 import { v4 } from "uuid";
@@ -6,6 +7,9 @@ import { NavBarProps } from "./NavBar.d";
 import "./NavBar.scss";
 
 export const NavBar = function(props: NavBarProps) {
+
+    const [ id ] = useState<string>(`${NavBar.name}-${v4()}`);
+
     return (
         <nav className="navbar px-3 py-2 navbar-expand-lg navbar-dark bg-dark fixed-top">
 
@@ -21,14 +25,14 @@ export const NavBar = function(props: NavBarProps) {
 
             <button className="navbar-toggler collapsed mx-3 px-2"
                 onBlur={(e) => {
-                    const div = document.querySelector(`#${props.id}`) as HTMLDivElement;
+                    const div = document.querySelector(`#${id}`) as HTMLDivElement;
                     if (div.className.includes("show")) {
                         e.target.dispatchEvent(new Event("click"));
                     }
                 }}
                 data-bs-toggle="collapse" 
-                data-bs-target={`#${props.id}`} 
-                aria-controls={`${props.id}`} 
+                data-bs-target={`#${id}`} 
+                aria-controls={`${id}`} 
                 aria-expanded="false" 
                 aria-label="Toggle navigation"
                 type="button">
@@ -37,7 +41,7 @@ export const NavBar = function(props: NavBarProps) {
                 </span>
             </button>
 
-            <div className="navbar-collapse collapse" id={props.id}>
+            <div className="navbar-collapse collapse" id={id}>
                 <div className="navbar-nav">
                     <Link className="nav-link mx-3" to={endpoints.educationPage}>Education</Link>
                     <Link className="nav-link mx-3" to={endpoints.projectsPage}>Projects</Link>
@@ -48,8 +52,4 @@ export const NavBar = function(props: NavBarProps) {
 
         </nav>
     );
-}
-
-NavBar.defaultProps = {
-    id: `${NavBar.name}-${v4()}`
 }
